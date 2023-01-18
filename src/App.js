c51732d7f283ee7100e09c94904ad05f23fe316d
brandmch <101467867+brandmch@users.noreply.github.com>
2023-01-18 11:57:56 -0500

    keybinding css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,30 @@ import "./App.css";
 import { useState } from "react";
 import { Parser } from "expr-eval";
 
+// All buttons on the calculator
+// Placed here to avoid manually creating <div>s in the return
+const buttons = [
+  ["all-clear r2 box", "AC"],
+  ["box r2", "/"],
+  ["box r2", "*"],
+  ["box r3", "7"],
+  ["box r3", "8"],
+  ["box r3", "9"],
+  ["box r3", "-"],
+  ["box r4", "4"],
+  ["box r4", "5"],
+  ["box r4", "6"],
+  ["box r4", "+"],
+  ["box r5", "1"],
+  ["box r5", "2"],
+  ["box r5", "3"],
+  ["equals box btm-right", "="],
+  ["zero r6 box btm-left", "0"],
+  ["box r6", "."],
+];
+
+// Maximum length of number on calculator is 14 digits
+// This checks if that is the case before displaying number
 function checkIfMoreThan14(num) {
   let tempNum = num;
   if (tempNum.toString().split("").length > 14) {
@@ -13,29 +37,43 @@ function checkIfMoreThan14(num) {
   return tempNum;
 }
 
+// Checks if pressed key is valid before passing to buttonPress()
+function isValidKey(key) {
+  let isValid;
+  switch (key) {
+    case ".":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+    case "0":
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+    case "Enter":
+    case "NumLock":
+    case "=":
+    case "AC":
+      isValid = true;
+      break;
+    default:
+      isValid = false;
+      break;
+  }
+  return isValid;
+}
+
 function App() {
+  // total = top number
+  // subTotal = bottom number
   const [total, setTotal] = useState(0);
   const [subTotal, setSubTotal] = useState(0);
-
-  const buttons = [
-    ["all-clear r2 box", "AC"],
-    ["box r2", "/"],
-    ["box r2", "*"],
-    ["box r3", "7"],
-    ["box r3", "8"],
-    ["box r3", "9"],
-    ["box r3", "-"],
-    ["box r4", "4"],
-    ["box r4", "5"],
-    ["box r4", "6"],
-    ["box r4", "+"],
-    ["box r5", "1"],
-    ["box r5", "2"],
-    ["box r5", "3"],
-    ["equals box btm-right", "="],
-    ["zero r6 box btm-left", "0"],
-    ["box r6", "."],
-  ];
 
   function buttonPress(key) {
     if (key === "Enter") {
@@ -96,31 +134,45 @@ function App() {
     setSubTotal(tempSubTotal);
   }
 
+  // Key Bindings
   window.document.onkeydown = (e) => {
-    switch (e.key) {
-      case ".":
-      case "1":
-      case "2":
-      case "3":
-      case "4":
-      case "5":
-      case "6":
-      case "7":
-      case "8":
-      case "9":
-      case "0":
-      case "+":
-      case "-":
-      case "*":
-      case "/":
+    if (isValidKey(e.key)) {
+      buttonPress(e.key);
+    }
+  };
+  function keydown({ key }) {
+    switch (key) {
       case "Enter":
+        key = "=";
+        break;
       case "NumLock":
-        buttonPress(e.key);
+        key = "AC";
         break;
       default:
         break;
     }
-  };
+    if (isValidKey(key)) {
+      document.getElementById(key).style.backgroundColor = "#00ff00";
+      document.getElementById(key).style.boxShadow = "none";
+    }
+  }
+  function keyup({ key }) {
+    switch (key) {
+      case "Enter":
+        key = "=";
+        break;
+      case "NumLock":
+        key = "AC";
+        break;
+      default:
+        break;
+    }
+    if (isValidKey(key)) {
+      document.getElementById(key).style = {};
+    }
+  }
+  document.addEventListener("keydown", keydown);
+  document.addEventListener("keyup", keyup);
 
   return (
     <div className="App">
