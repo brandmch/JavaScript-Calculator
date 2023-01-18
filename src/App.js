b0c2b6ba17751434372333010d62c24510e71097
brandmch <101467867+brandmch@users.noreply.github.com>
2023-01-18 12:05:26 -0500

    created utils

--- src/App.js
+++ src/App.js
@@ -1,73 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import { Parser } from "expr-eval";
-
-// All buttons on the calculator
-// Placed here to avoid manually creating <div>s in the return
-const buttons = [
-  ["all-clear r2 box", "AC"],
-  ["box r2", "/"],
-  ["box r2", "*"],
-  ["box r3", "7"],
-  ["box r3", "8"],
-  ["box r3", "9"],
-  ["box r3", "-"],
-  ["box r4", "4"],
-  ["box r4", "5"],
-  ["box r4", "6"],
-  ["box r4", "+"],
-  ["box r5", "1"],
-  ["box r5", "2"],
-  ["box r5", "3"],
-  ["equals box btm-right", "="],
-  ["zero r6 box btm-left", "0"],
-  ["box r6", "."],
-];
-
-// Maximum length of number on calculator is 14 digits
-// This checks if that is the case before displaying number
-function checkIfMoreThan14(num) {
-  let tempNum = num;
-  if (tempNum.toString().split("").length > 14) {
-    tempNum = tempNum.toFixed(4);
-    if (tempNum.toString().split("").length > 14) {
-      tempNum = parseInt(tempNum).toExponential(2);
-    }
-  }
-  return tempNum;
-}
-
-// Checks if pressed key is valid before passing to buttonPress()
-function isValidKey(key) {
-  let isValid;
-  switch (key) {
-    case ".":
-    case "1":
-    case "2":
-    case "3":
-    case "4":
-    case "5":
-    case "6":
-    case "7":
-    case "8":
-    case "9":
-    case "0":
-    case "+":
-    case "-":
-    case "*":
-    case "/":
-    case "Enter":
-    case "NumLock":
-    case "=":
-    case "AC":
-      isValid = true;
-      break;
-    default:
-      isValid = false;
-      break;
-  }
-  return isValid;
-}
+import { buttons, checkIfMoreThan14, isValidKey } from "./utils/utils";
 
 function App() {
   // total = top number
@@ -183,6 +117,7 @@ function App() {
             <p className="total">{subTotal}</p>
           </div>
         </div>
+        {/* Array of buttons imported from utils */}
         {buttons.map((curr) => (
           <div
             key={curr[1]}
