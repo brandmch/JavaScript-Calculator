c72a6b992d1777d79b2f3b054ce31c3be50ae45a
brandmch <101467867+brandmch@users.noreply.github.com>
2023-01-17 23:39:14 -0500

    . added

--- src/App.js
+++ src/App.js
@@ -1,32 +1,82 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [subTotal, setSubTotal] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  const buttons = [
+    ["all-clear r2 box", "AC"],
+    ["box r2", "/"],
+    ["box r2", "*"],
+    ["box r3", "7"],
+    ["box r3", "8"],
+    ["box r3", "9"],
+    ["box r3", "-"],
+    ["box r4", "4"],
+    ["box r4", "5"],
+    ["box r4", "6"],
+    ["box r4", "+"],
+    ["box r5", "1"],
+    ["box r5", "2"],
+    ["box r5", "3"],
+    ["equals box", "="],
+    ["zero r6 box", "0"],
+    ["box r6", "."],
+  ];
+
+  function buttonPress(key) {
+    if (total === 0) {
+      setTotal(key);
+    } else {
+      switch (key) {
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+        case "0":
+          setTotal(`${total}${key}`);
+          break;
+        case ".":
+          if (!/[^0-9]/.test(total)) {
+            setTotal(`${total}.`);
+          }
+          break;
+        case "+":
+        case "-":
+        case "*":
+        case "/":
+        case "=":
+        case "AC":
+          setTotal(0);
+      }
+    }
+  }
+
   return (
     <div className="App">
       <div className="calculator-container">
         <div className="screen r1">
           <div>
-            <p style={{ fontSize: "large" }}>0</p>
-            <p className="total">0</p>
+            <p style={{ fontSize: "large" }}>{subTotal}</p>
+            <p className="total">{total}</p>
           </div>
         </div>
-        <div className="all-clear r2 box">AC</div>
-        <div className="box r2">/</div>
-        <div className="box r2">X</div>
-        <div className="box r3">7</div>
-        <div className="box r3">8</div>
-        <div className="box r3">9</div>
-        <div className="box r3">-</div>
-        <div className="box r4">4</div>
-        <div className="box r4">5</div>
-        <div className="box r4">6</div>
-        <div className="box r4">+</div>
-        <div className="box r5">1</div>
-        <div className="box r5">2</div>
-        <div className="box r5">3</div>
-        <div className="equals box">=</div>
-        <div className="zero r6 box">0</div>
-        <div className="box r6">.</div>
+        {buttons.map((curr) => (
+          <div
+            key={curr[1]}
+            className={curr[0]}
+            id={curr[1]}
+            onClick={(e) => buttonPress(e.target.id)}
+          >
+            {curr[1]}
+          </div>
+        ))}
       </div>
     </div>
   );
